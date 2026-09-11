11d2be7502e5b9922dec505efe43e8691bc29372
Juan Vasquez <juand.delcampo01@gmail.com>
2026-09-11 11:38:18 -0500

    agregar registro de fechas

--- Gesntion de Gastos/README.md
+++ Gesntion de Gastos/README.md
@@ -1,4 +1,6 @@
 Proyecto para gestionar los gastos e ingresos en Python.
 
-Version V0.1
-    Regitra y muestra los ingresos y egresos totales junto a una descripcion de monto ingresado
+Version V0.1.0
+    Regitra y muestra los ingresos y egresos totales junto a una descripcion de monto ingresado.
+Version V0.2.0
+    Registro y validacion de fechas.
--- Gesntion de Gastos/gestion_gastp.py
+++ Gesntion de Gastos/gestion_gastp.py
@@ -1,3 +1,6 @@
+import json as js
+import datetime
+
 total_ingresos = 0
 total_egresos = 0
 saldo = 0
@@ -16,38 +19,61 @@ def ingresar_monto():
         except ValueError:
             print("Entrada inválida. Por favor, ingrese un valor valido.\n")
 
+def validar_fechaingresa(fecha_texto):
+    try:
+        datetime.datetime.strptime(fecha_texto, "%d/%m/%Y")
+        return True
+    except ValueError:
+        return 
+
+def ingresar_fecha():
+    fecha = input("Ingrese la fecha (dd/mm/aaaa): ")
+    while not validar_fechaingresa(fecha):
+        print("Fecha inválida. Por favor, ingrese una fecha válida en el formato dd/mm/aaaa.")
+        fecha = input("Ingrese la fecha (dd/mm/aaaa): ")
+    return fecha
+
 while True:
     opcion = input("Ingrese una opción:\n" \
     "1. Registrar Ingreso\n" \
     "2. Registrar Egreso\n" \
-    "3. Mostrar Gastos\n" \
-    "4. Mostrar Montos Totales\n" \
-    "5. Salir\n")
+    "3. Mostrar Ingresos\n" \
+    "4. Mostrar Gastos\n" \
+    "5. Mostrar Montos Totales\n" \
+    "6. Salir\n")
 
     if opcion == "1":
         print("\n-----Registrar Ingreso-----")
+        fecha = ingresar_fecha()
         monto, descripcion = ingresar_monto()
-        ingreso.append((descripcion, monto))
+        ingreso.append((fecha, descripcion, monto))
         total_ingresos += monto
         saldo += monto
+        print(f"Ingreso registrado: {fecha} - {descripcion}: S/. {monto}\n")
 
     elif opcion == "2":
         print("\n-----Registrar Egreso-----")
+        fecha = ingresar_fecha()
         monto, descripcion = ingresar_monto()
-        gastos.append((descripcion, monto))
+        gastos.append((fecha, descripcion, monto))
         total_egresos += monto
         saldo -= monto
-
+        print(f"Egreso registrado: {fecha} - {descripcion}: S/. {monto}\n")
     elif opcion == "3":
-        print(f"\n-----Gastos-----\n")
-        for descripcion, monto in gastos:
-            print(f"{descripcion}: S/. {monto}")
+        print(f"\n-----Ingresos-----\n")
+        for fecha, descripcion, monto in ingreso:
+            print(f"{fecha} - {descripcion}: S/. {monto}\n")
+
     elif opcion == "4":
+        print(f"\n-----Gastos-----\n")
+        for fecha, descripcion, monto in gastos:
+            print(f"{fecha} - {descripcion}: S/. {monto}\n")
+    elif opcion == "5":
         print(f"\n-----Montos Totales-----\n")
         print(f"Total Ingresos: S/. {total_ingresos}")
         print(f"Total Egresos: S/. {total_egresos}")
-        print(f"Saldo: S/. {saldo}")
-    elif opcion == "5":
+        print(f"Saldo: S/. {saldo}\n")
+    elif opcion == "6":
         print("Saliendo del programa...")
         break
     else:
